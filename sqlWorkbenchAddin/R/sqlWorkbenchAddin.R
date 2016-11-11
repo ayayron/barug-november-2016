4e07e47441892b5bf6842189b70cdc26b68b61a8
Aaron Hoffer <ahoffer42@gmail.com>
2016-11-10 22:19:02 -0800

    2. Hello world with gadget UI

--- sqlWorkbenchAddin/R/sqlWorkbenchAddin.R
+++ sqlWorkbenchAddin/R/sqlWorkbenchAddin.R
@@ -1,3 +1,23 @@
+#'@exportHello World
+
 sqlWorkbenchAddin <- function() {
-	rstudioapi::insertText("Hello World")
+  library(shiny)
+  library(miniUI)
+  
+  ui <- miniPage(
+    gadgetTitleBar("SQL Workbench")
+  )
+  
+  server <- function(input, output, session) {
+    
+    # when "Done" is clicked return the text
+    observeEvent(input$done, {
+      stopApp(rstudioapi::insertText("Hello World"))
+    })
+  }
+
+  # create a dialog window instead of having it return in the viewer pane
+  viewer <- dialogViewer("sqlWorkbench", width = 800, height = 800) 
+  
+  runGadget(ui, server, viewer = viewer)
 }
