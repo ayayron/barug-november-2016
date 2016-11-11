3abe1346e23afae37e4642a5a01617ddc87c901d
Aaron Hoffer <ahoffer42@gmail.com>
2016-11-11 10:34:35 -0800

    5. Add a plotting window with drop downs

--- sqlWorkbenchAddin/R/sqlWorkbenchAddin.R
+++ sqlWorkbenchAddin/R/sqlWorkbenchAddin.R
@@ -1,9 +1,13 @@
-#'@exportHello World
+#'@export
 
 sqlWorkbenchAddin <- function() {
   library(shiny)
   library(miniUI)
+  library(ggplot2)
   
+  # get verbose output from shiny for debugging
+  options(shiny.trace = TRUE)
+
   ui <- miniPage(
     gadgetTitleBar("SQL Workbench"),
     miniTabstripPanel(
@@ -16,6 +20,15 @@ sqlWorkbenchAddin <- function() {
             fillRow(textInput("table_filter", "Table Filter"), 
                     actionButton("filter_btn", "Filter!"), height = "75px"),
             fillRow(dataTableOutput("sql_table"), height = "300px")
+      ),
+      miniTabPanel("Chart", icon = icon("line-chart"),
+                   fillPage(fillRow(
+                     fillCol(selectInput("x_select", "Select X Value", choices = c("None")),
+                             selectInput("y_select", "Select Y Value", choices = c("None")),
+                             selectInput("plot_type","Plot Type", choices = c("line"="geom_line()",
+                                                                              "point"="geom_point()"))),
+                     plotOutput("sql_plot"), flex = c(1,4)
+                   ))
       )
     )
   )
@@ -31,7 +44,11 @@ sqlWorkbenchAddin <- function() {
     })
     
     observeEvent(input$filter_btn,{
-      df$table = subset(df$table, eval(parse(text = input$table_filter)))
+      df$table = tryCatch({
+                    subset(df$table, eval(parse(text = input$table_filter)))
+                  }, error = function(e) {
+                        df$table      
+                  })
     })
     
     # Output a DataTable (js package, not data.table R package)
@@ -40,6 +57,30 @@ sqlWorkbenchAddin <- function() {
       df$table,
       options = list(pageLength = 5, scrollY = 300)
     )
+    
+    # update filters for plot
+    observe({
+        updateSelectInput(session, "x_select", choices = names(df$table))
+        updateSelectInput(session, "y_select", choices = names(df$table))
+    })
+    
+    output$sql_plot = renderPlot({
+      
+      # eval used to parse select input text as columns of data frame
+      p = ggplot(df$table, aes(x = eval(parse(text = input$x_select)),
+                              y = eval(parse(text = input$y_select)))) + 
+                ylab(input$y_select) + xlab(input$x_select)
+      
+      # based on select input set the type of plot
+      # the whole renderPlot() is re-evaluated every time the input changes
+      if (input$plot_type == "geom_line()") {
+        p = p + geom_line()
+      }
+      if (input$plot_type == "geom_point()") {
+        p = p + geom_point()
+      }
+      p
+    })
     
     # when "Done" is clicked return the text
     observeEvent(input$done, {
