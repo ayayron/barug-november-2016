f349813fdeefe2740e3e5c80871c2fb5cb374365
Aaron Hoffer <ahoffer42@gmail.com>
2016-11-10 23:33:54 -0800

    4. Create table view and add a filter

--- sqlWorkbenchAddin/R/sqlWorkbenchAddin.R
+++ sqlWorkbenchAddin/R/sqlWorkbenchAddin.R
@@ -11,21 +11,39 @@ sqlWorkbenchAddin <- function() {
           miniContentPanel(
             fileInput("load_file", "Load File")
           )
+      ),
+      miniTabPanel("Table", icon = icon("table"),
+            fillRow(textInput("table_filter", "Table Filter"), 
+                    actionButton("filter_btn", "Filter!"), height = "75px"),
+            fillRow(dataTableOutput("sql_table"), height = "300px")
       )
     )
   )
   
   server <- function(input, output, session) {
     
+    # create our reactive data frame to be used later
+    df = reactiveValues(table = NULL)
+    
     # read the file when it is uploaded
-    return_df = eventReactive(input$load_file,{
-      df = readRDS(input$load_file$datapath)
-      return(df)
+    observeEvent(input$load_file,{
+      df$table = readRDS(input$load_file$datapath)
     })
+    
+    observeEvent(input$filter_btn,{
+      df$table = subset(df$table, eval(parse(text = input$table_filter)))
+    })
+    
+    # Output a DataTable (js package, not data.table R package)
+    # set options https://datatables.net/reference/option/
+    output$sql_table = renderDataTable(
+      df$table,
+      options = list(pageLength = 5, scrollY = 300)
+    )
     
     # when "Done" is clicked return the text
     observeEvent(input$done, {
-      stopApp(return_df())
+      stopApp(df$table)
     })
   }
 
